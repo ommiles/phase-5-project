fc4e14ffdace9c3e42e4bac6137f65f978a51e82
OM Miles <miles@ommiles.com>
2021-06-30 09:36:28 -0700

    rails new backend --api -d postgresql

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { withRouter } from "react-router-dom";
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Navigation from "./Navigation";
-import Name from "./Name";
+import NotFound from "./NotFound";
 
 class App extends Component {
   state = {
     user: "",
     comments: [],
   };
+
+  // Toggle logged in and out states and render
+  // const [login, setLogin] = useState(false);
+  // <button onClick={() => setLogin(!login)}>{login ? "Logout" : "Login"}</button>
 
   handleLogout = () => {};
 
@@ -19,12 +23,36 @@ class App extends Component {
 
   componentDidMount() {}
 
+  useEffect(() => {
+    window.scroll(0,0)
+  }, []);
+
+  // how to fetch id from URL:
+  // import { useParams } from 'react-router-dom';
+  // const { id } = useParams()
+
+  // const query = new URLSearchParams(useLocation().search)
+  // this hook includes pathname and search props
+  // <h2>{query.get("first")}</h2>
+  // <h2>{query.get("last")}</h2>
+
   render() {
     return;
     <Router basename="patreon">
       <Navigation />
       <Route path="/" component={Name} exact />
       <Route path="/login" component={Name} />
+      <Route path="/signup" component={Name} />
+      <Route path="/user/:name" component={Name} />
+      <Route path="/creator/:name" component={Name} />
+      <Route path="/profile">
+        {/* use a useEffect hook with useHistory hook in Profile component */}
+        {/* const history = useHistory */}
+        {/* useEffect(() => { if(!login) {history.push('/')}}, []); */}
+        <Profile login={login} />
+        {/* { login ? <Profile/> : <Redirect to="/"/> } */}
+      </Route>
+      <Route component={NotFound} />
     </Router>;
   }
 }
