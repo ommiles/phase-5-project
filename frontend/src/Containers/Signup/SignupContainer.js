4f6e485f520026b2c451ec5659741c3d7a1413c9
OM Miles <miles@ommiles.com>
2021-07-08 13:57:52 -0700

    note on signup added

--- frontend/src/Containers/Signup/SignupContainer.js
+++ frontend/src/Containers/Signup/SignupContainer.js
@@ -5,6 +5,7 @@ export default function SignupContainer(props) {
   return (
     <div>
       <h1>This is the Signup Container.</h1>
+      {/* if there is a current user logged in, no form is rendered */}
       {Object.keys(props.currentUser).length > 0 ? <h1>You already have an account, silly :)</h1> : <SignupForm />}
     </div>
   );
