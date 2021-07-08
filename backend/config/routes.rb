77c8a02dc142a9b307e51f2058856c19235bcb55
OM Miles <miles@ommiles.com>
2021-07-08 13:36:03 -0700

    update seeds, db, and password column on user table

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :update, :destroy]
+      resources :users, only: [:index, :create, :update, :destroy]
       resources :posts
       resources :subscriptions
       resources :comments
