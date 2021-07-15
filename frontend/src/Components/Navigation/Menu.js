7c449fa9c188c0eb71264a59f6c3906f808151fa
OM Miles <miles@ommiles.com>
2021-07-14 22:41:05 -0700

    menu routing completed

--- frontend/src/Components/Navigation/Menu.js
+++ frontend/src/Components/Navigation/Menu.js
@@ -1,19 +1,60 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { toggleHomeState, toggleLoginState, toggleSignupState } from '../../Actions/toggleAction';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  toggleHomeState,
+  toggleLoginState,
+  toggleMenu,
+  toggleSignupState,
+} from '../../Actions/toggleAction';
+import { useHistory } from 'react-router';
 
 export default function Menu() {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const currentUser = useSelector(state => state.login.currentUser);
+  const handleHome = () => {
+    Object.keys(currentUser).length > 0
+      ? history.push('/home')
+      : history.push('/');
+    dispatch(toggleMenu());
+    // dispatch(toggleHomeState())
+  };
+  const handleLogin = () => {
+    history.push('/login');
+    dispatch(toggleMenu());
+    // dispatch(toggleLoginState())
+  };
+  const handleSignup = () => {
+    history.push('/signup');
+    dispatch(toggleMenu());
+    // dispatch(toggleSignupState())
+  };
+
   return (
     <div className='mt5 mb5 w-100 fixed flex right-0 bottom-0 top-0 flex-column justify-around items-center'>
       <div>
-        <a className='f1 pv5 pointer w-100 bb black domaine-sans-fine-thin' onClick={() => dispatch(toggleHomeState())} >Home</a>
+        <a
+          className='f1 pv5 pointer w-100 bb black domaine-sans-fine-thin'
+          onClick={handleHome}
+        >
+          Home
+        </a>
       </div>
       <div>
-        <a className='f1 pv5 pointer w-100 bb black domaine-sans-fine-thin' onClick={() => dispatch(toggleLoginState())}>Login</a>
+        <a
+          className='f1 pv5 pointer w-100 bb black domaine-sans-fine-thin'
+          onClick={handleLogin}
+        >
+          Login
+        </a>
       </div>
       <div>
-        <a className='f1 pv5 pointer w-100 black domaine-sans-fine-thin' onClick={() => dispatch(toggleSignupState())}>Sign Up</a>
+        <a
+          className='f1 pv5 pointer w-100 black domaine-sans-fine-thin'
+          onClick={handleSignup}
+        >
+          Sign Up
+        </a>
       </div>
     </div>
   );
